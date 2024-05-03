f30fac42ae7ae5e67d6e3b8b75cac0bb7edc86e3
Ronielsom <ronielson7297@gmail.com>
2024-05-03 00:08:44 -0300

    TV

--- Meu-Primeiro-Projeto-Java/src/edu/ronielson/primeiraSeman/SmartTvProjeto/SmartTv.java
+++ Meu-Primeiro-Projeto-Java/src/edu/ronielson/primeiraSeman/SmartTvProjeto/SmartTv.java
@@ -7,11 +7,31 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+        //System.out.println("Aumentando Canal: " + canal);
+    }
+    public void diminuirCanal(){
+        canal--;
+        //System.out.println("Diminuindo Canal: " + canal);
+    }
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando volume para: " + volume);
+    }
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo volume para: " + volume);
+    }
+    
     public void ligar(){
         ligada = true;
     }
     public void desligar(){
         ligada = false;
     }
-    }
+}
 
--- Meu-Primeiro-Projeto-Java/src/edu/ronielson/primeiraSeman/SmartTvProjeto/UsuarioTv.java
+++ Meu-Primeiro-Projeto-Java/src/edu/ronielson/primeiraSeman/SmartTvProjeto/UsuarioTv.java
@@ -6,11 +6,26 @@ public class UsuarioTv {
     public static void main(String[] args) throws Exception{
         SmartTv smartTv= new SmartTv();
 
+        smartTv.diminuirVolume();
+        
+        smartTv.diminuirVolume();
+        
+        smartTv.diminuirVolume();
+        
+        smartTv.aumentarVolume();
+
+        System.out.println("Volume Atual : " + smartTv.volume);
+        System.out.println("Canal atual ?" + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual ?" + smartTv.canal);
         System.out.println("TV Ligada ?" + smartTv.ligada);
         System.out.println("Canal atual ?" + smartTv.canal);
         System.out.println("Volume Atual ?" + smartTv.volume);
 
         smartTv.ligar();
+        System.out.println("Novo Status-> TV Ligada ?" + smartTv.ligada);
+
+        smartTv.desligar();
         System.out.println("Novo Status-> TV Ligada ?" + smartTv.ligada);
 
     }
